ff8585ce698db11ffbc3a04970ecd507719091b1
Ambient Labs <178958084+ambient-labs@users.noreply.github.com>
2024-09-24 10:00:42 -0400

    Inline shoelace config

--- packages/docoddity/src/themes/default/scripts/index.ts
+++ packages/docoddity/src/themes/default/scripts/index.ts
@@ -1,3 +1,6 @@
+import { setBasePath } from 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace/dist/utilities/base-path.js';
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace/cdn');
+
 import './nav.js';
 import './page.js';
 import './head.js';
