75d902eebb10356185612b904effeff3c691ee2c
Ambient Labs <178958084+ambient-labs@users.noreply.github.com>
2024-09-24 10:05:53 -0400

    Fix shoelace import

--- packages/docoddity/src/themes/default/scripts/index.ts
+++ packages/docoddity/src/themes/default/scripts/index.ts
@@ -1,4 +1,13 @@
-import { setBasePath } from 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace/dist/utilities/base-path.js';
+// import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+// setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace/cdn');
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/themes/dark.css';
+// import '@shoelace-style/shoelace/dist/components/button/button.js';
+// import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+// import '@shoelace-style/shoelace/dist/components/input/input.js';
+// import '@shoelace-style/shoelace/dist/components/rating/rating.js';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace/cdn');
 
 import './nav.js';
