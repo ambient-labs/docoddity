1ac60b9a105c4842ed5276f7d9b9fb2b7834bfdf
Ambient Labs <178958084+ambient-labs@users.noreply.github.com>
2024-09-28 14:11:07 -0400

    Fix bug with page titles

--- packages/docoddity/src/bin/lib/utils/build-page-node-url.ts
+++ packages/docoddity/src/bin/lib/utils/build-page-node-url.ts
@@ -1,7 +1,7 @@
-export const buildPageNodeURL = (parts: string[]) => {
+export const buildPageNodeURL = (parts: string[]): string => {
   const lastPart = parts[parts.length - 1].replace(/\.(md|html)$/, '');
   return '/' + [
     ...parts.slice(0, -1),
-    lastPart === 'index' ? undefined : lastPart,
+    lastPart === 'index' ? '' : lastPart,
   ].join('/');
 };
